f2436cdfd05bcec39993fae130a94b989ceefd2d
Yevhenii Pozniak <jekamanu@gmail.com>
2025-08-22 13:03:40 +0300

    update

---
+++ lesson-4/03-body/index.js
@@ -0,0 +1,28 @@
+// файл POST відправлення наших даних
+// {
+//     "title": "Film 1",
+//     "producer": "Producer 1",
+//     "year": 2000
+// }
+
+import express from "express";
+
+import crypto from "node:crypto";
+
+const PORT = 8080;
+const app = express();
+
+// глобальна мідлвара, (краще її парсити в не глобально) розпарсюємо наш JSON файл який ми передали, щоб побачити його в консолі
+//! app.use(express.json()); //побачимо в консолі { title: 'Film 1', producer: 'Producer 1', year: 2000 }
+
+const jsonParser = express.json();
+
+app.post("/movies", jsonParser, (req, res) => {
+  //   console.log(req.body);
+  const { title, producer, year } = req.body;
+  res.status(201).send({ id: crypto.randomUUID(), title, producer, year }); //201 - created та повертаємо весь об'єкт який створили (додаючи наш id)
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ lesson-4/04-validation/index.js
@@ -0,0 +1,45 @@
+//! тут ми додаємо валідацію на title, producer, year які приходять в Body через бібліотеку Joi (аналог Yup)
+
+// файл POST відправлення наших даних
+// {
+//     "title": "Film 1",
+//     "producer": "Producer 1",
+//     "year": 2000
+// }
+
+import express from "express";
+
+import crypto from "node:crypto";
+import morgan from "morgan";
+
+import movieSchema from "./schema/movie.js";
+
+const PORT = 8080;
+const app = express();
+
+// для логування в консолі
+app.use(morgan("combined"));
+
+const jsonParser = express.json();
+
+app.post("/movies", jsonParser, (req, res) => {
+  //   console.log(req.body);
+  const movie = {
+    title: req.body.title,
+    producer: req.body.producer,
+    year: req.body.year,
+  };
+
+  // виконуємо валідацію, convert: false (щоб не було конвертації)
+  const { error, value } = movieSchema.validate(movie, { convert: false });
+  if (typeof error !== "undefined") {
+    return res.status(400).send("Validation Error");
+  }
+
+  res.status(201).send({ id: crypto.randomUUID(), ...value }); //201 - created та повертаємо весь об'єкт який створили (додаючи наш id)
+  res.end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
